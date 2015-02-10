efcb553271c17d44c1b86a7ea569caf131948f76
Ryan Hamblin <ryanhca@gmail.com>
2015-02-09 20:29:33 -0700

    added a service trying to link it all up

--- friendCtrl.js
+++ friendCtrl.js
@@ -1,8 +1,12 @@
 var app = angular.module('ngApp');
 // Create a FriendController in the FriendController.js file
-app.controller('friendCtrl', function($scope) {
+app.controller('friendCtrl', function($scope, friendService) {
 	$scope.totes = 'Totes Mcgoats!';
 
+	// * In the FriendController copy the data from friend-data.json into a scope variable called "friends" in your FriendController
+	$scope.searchTerm = '';
+
+	$scope.friends = friendService.getfriends();
 });
 
 
