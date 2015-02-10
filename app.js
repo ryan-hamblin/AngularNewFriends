8681e0e28eef330e063b83837c2bd7e36c14ab79
Ryan Hamblin <ryanhca@gmail.com>
2015-02-09 20:10:31 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,2 @@
+var app = angular.module("ngApp", []);
+// * Create an app variable for your angular app in app.js (this should be one line). You can name your app whatever your want
---
+++ friendCtrl.js
@@ -0,0 +1,10 @@
+var app = angular.module('ngApp');
+// Create a FriendController in the FriendController.js file
+app.controller('friendCtrl', function($scope) {
+	$scope.totes = 'Totes Mcgoats!';
+
+});
+
+
+
+
