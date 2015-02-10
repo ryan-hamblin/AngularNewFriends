eb4a6f4cdf41627a9f602030e31ba84bf6a35458
Ryan Hamblin <ryanhca@gmail.com>
2015-02-09 20:48:20 -0700

    added some janky styles. Commented out the service because it wasn't working.

--- friendCtrl.js
+++ friendCtrl.js
@@ -1,12 +1,12 @@
 var app = angular.module('ngApp');
 // Create a FriendController in the FriendController.js file
-app.controller('friendCtrl', function($scope, friendService) {
+app.controller('friendCtrl', function($scope) {
 	$scope.totes = 'Totes Mcgoats!';
 
 	// * In the FriendController copy the data from friend-data.json into a scope variable called "friends" in your FriendController
 	$scope.searchTerm = '';
 
-	$scope.friends = friendService.getfriends();
+	// $scope.friends = friendService.getfriends();
 });
 
 
